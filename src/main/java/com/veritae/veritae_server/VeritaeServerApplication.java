e3e01d7fe8d5ba5bfdf814731e1928c1a5180cb9
sungbeenCho <sbc001121@gmail.com>
2026-05-27 19:06:26 +0900

    init: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/veritae/veritae_server/VeritaeServerApplication.java
@@ -0,0 +1,13 @@
+package com.veritae.veritae_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeritaeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeritaeServerApplication.class, args);
+	}
+
+}
